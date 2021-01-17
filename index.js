d54b10d9039f45d2d3bc2407ede0a9af3a57efc0
einst.alfimi <einst.4lchemie@gmail.com>
2021-01-17 18:06:55 +0900

    make user migrate/seeder, add login logic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
+const bcrypt = require('bcrypt');
+const User = require('./models').User;
 
 const http = require('http');
 const app = express();
@@ -13,9 +15,22 @@ app.get('/', (req, res) => {
     console.log('home');
     res.sendStatus(200);
 });
-app.post('/login', (req, res) => {
-    console.log(req.body);
-    res.status(200).send({status: 'ok'});
+app.post('/login', async (req, res) => {
+    const user = await User.findOne({where: {name: req.body.id} });
+    if (!user) {
+        console.log("id ng");
+        res.status(401).send({status: 'ng'});
+        return;
+    }
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+        console.log("password NG");
+        res.status(401).send({status: 'ng'});
+        return;
+    } else {
+        console.log(`${req.body.id} is logged in.`);
+        res.status(200).send({status: 'ok'});
+        return;
+    }
 });
 
 app.get('/score/:sha256', (req, res) => {
---
+++ seeders/20210116124953-test-users.js
@@ -0,0 +1,16 @@
+'use strict';
+const bcrypt = require('bcrypt');
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const now = new Date();
+    const myPlaintextPassword = 'test-password';
+    const saltRounds = 10;
+    const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+    // TODO uniqueIDの生成方法は考える
+    return queryInterface.bulkInsert('Users', [
+      { name: 'test-user',  unique: 'uniqueid', password: hash, createdAt: now, updatedAt: now},
+    ], {});
+  },
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});  }
+};
